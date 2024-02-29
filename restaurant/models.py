88504562c2a6ebdebb08b2b944e86a72b14461ed
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-02-29 14:36:09 +0000

    Define 'HomeText' model for the text on the landingpage, register the model in admin.py and add templates folder to restaurant app

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class HomeText(models.Model):
+    title = models.CharField(max_length = 50)
+    introduction = models.TextField(max_length = 700)
+
+
+    def __str__(self):
+        return self.title
+        
